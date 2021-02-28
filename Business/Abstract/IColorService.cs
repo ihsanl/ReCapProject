ff816421ed8662dbd6db36865b91845823bceea4
ihsnl <ihsanlevent1@gmail.com>
2021-02-28 22:48:19 +0300

    Add project files.

---
+++ Business/Abstract/IColorService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IColorService
+    {
+        IDataResult<List<Color>> GetAll();
+        IDataResult<Color> Get(int id);
+        IResult Add(Color entity);
+        IResult Update(Color entity);
+        IResult Delete(Color entity);
+    }
+}
---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,37 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Business.Abstract;
+using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //Car
+            //Color
+            builder.RegisterType<ColorManager>().As<IColorService>();
+            builder.RegisterType<EfColorDal>().As<IColorDal>();
+            //Customer
+
+            //builder.RegisterType<HttpContextAccessor>().As<IHttpContextAccessor>();
+
+            //Aspects Load
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
+        }
+    }
+}
